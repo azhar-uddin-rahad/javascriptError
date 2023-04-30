a646ca15205c37a6704888be667e4b8f674f280d
azharrahad31 <azherudinrahad@gmail.com>
2023-04-30 11:09:24 +0600

    range error and referanceError created

---
+++ error.js
@@ -0,0 +1,15 @@
+//range error;
+const arr=[1,2,3];
+//error when a value is not in the set or range of allowed values.
+//arr.length=90**90;
+//console.log(arr.length);
+
+
+
+//referenceError 
+// when variable not declare but can access this not declarable variable this thime show a referenceError 
+
+//money is not declare
+console.log(money)
+
+
